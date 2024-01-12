3ea79df9beafc1ed00f8b6917c1c12e58c988ecc
mhashemm <msakr456@gmail.com>
2024-01-13 00:04:34 +0200

    fonts and border

--- config.h
+++ config.h
@@ -8,9 +8,9 @@
 static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"NotoColorEmoji:pixelsize=10:antialias=true:autohint=true",
+	"NotoColorEmoji:pixelsize=14:antialias=true:autohint=true",
 };
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
